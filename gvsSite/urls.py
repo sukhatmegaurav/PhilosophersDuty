bcda30ce014d59faa54b7c9ca2a0f93ce01a2cb2
Gaurav Sukhatme <sukhatmegaurav@gmail.com>
2018-08-14 22:38:27 +0530

    fixed some errors

--- gvsSite/urls.py
+++ gvsSite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, re_path
 from blog import views as blog_views
 
 urlpatterns = [
